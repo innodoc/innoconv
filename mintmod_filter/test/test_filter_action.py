11e52089f6139aab01ad598166c257bd6ccf462b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-02-19 13:32:46 +0100

    refactor test_filter_action.py

--- mintmod_filter/test/test_filter_action.py
+++ mintmod_filter/test/test_filter_action.py
@@ -6,45 +6,42 @@ from mintmod_filter.filter_action import FilterAction, CLASS_UNKNOWN_CMD
 class TestFilterAction(unittest.TestCase):
     def setUp(self):
         self.fa = FilterAction()
-        self.elem_str = pf.Str('foo')
-        self.elem_math = pf.Math(r'1+2')
-        self.elem_mtitle = pf.RawBlock('\MTitle{Foo}', format='latex')
-        self.elem_unkown_cmd = pf.RawBlock(
-            '\ThisCommandDoesNotExist{Foo}', format='latex')
-        self.elem_unkown_raw_block = pf.RawBlock(
-            'NotAValidLatexCommand', format='latex')
-        block = pf.Block(
-            self.elem_str,
-            self.elem_math,
-            self.elem_mtitle,
-            self.elem_unkown_cmd,
-            self.elem_unkown_raw_block,
-        )
-        self.doc = pf.Doc(block)
+        self.doc = pf.Doc()
 
     def test_str_input(self):
         "filter() returns None if given Str element"
-        ret = self.fa.filter(self.elem_str, self.doc)
+        elem_str = pf.Str('foo')
+        ret = self._filter_elem([pf.Para(elem_str)], elem_str)
         self.assertIsNone(ret)
 
     def test_math_input(self):
         "filter() returns Math if given Math element"
-        ret = self.fa.filter(self.elem_math, self.doc)
+        elem_math = pf.Math(r'1+2')
+        ret = self._filter_elem([pf.Para(elem_math)], elem_math)
         self.assertEqual(type(ret), pf.Math)
 
     def test_known_latex_rawblock_command(self):
         "filter() handles known LaTeX command"
-        ret = self.fa.filter(self.elem_mtitle, self.doc)
+        elem_mtitle = pf.RawBlock('\MTitle{Foo}', format='latex')
+        ret = self._filter_elem([elem_mtitle], elem_mtitle)
         self.assertEqual(type(ret), pf.Header)
 
     def test_unknown_latex_rawblock_input(self):
         "filter() handles unknown LaTeX command"
-        div = self.fa.filter(self.elem_unkown_cmd, self.doc)
-        self.assertEqual(type(div), pf.Div)
-        self.assertIn(CLASS_UNKNOWN_CMD, div.classes)
-        self.assertIn('thiscommanddoesnotexist', div.classes)
+        elem_unkown_cmd = pf.RawBlock(
+            '\ThisCommandDoesNotExist{Foo}', format='latex')
+        ret = self._filter_elem([elem_unkown_cmd], elem_unkown_cmd)
+        self.assertEqual(type(ret), pf.Div)
+        self.assertIn(CLASS_UNKNOWN_CMD, ret.classes)
+        self.assertIn('thiscommanddoesnotexist', ret.classes)
 
     def test_unhandled_rawblock_input(self):
         "filter() handles unknown RawBlock"
-        ret = self.fa.filter(self.elem_unkown_raw_block, self.doc)
+        elem_unkown_raw_block = pf.RawBlock(
+            'NotAValidLatexCommand', format='latex')
+        ret = self._filter_elem([elem_unkown_raw_block], elem_unkown_raw_block)
         self.assertIsNone(ret)
+
+    def _filter_elem(self, elem_list, test_elem):
+        self.doc.content.extend(elem_list)
+        return self.fa.filter(test_elem, self.doc)
