65f2251e314efd4b712899e48b3457d425e2c8ba
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-02-21 15:59:57 +0100

    add test for handle_msref

--- mintmod_filter/test/test_commands.py
+++ mintmod_filter/test/test_commands.py
@@ -7,17 +7,23 @@ class TestCommands(unittest.TestCase):
 
     def setUp(self):
         self.commands = Commands()
+        self.doc = pf.Doc()
 
     def test_handle_msection(self):
-        doc = pf.Doc()
-        ret = self.commands.handle_msection(["A Test Title"], None, doc)
+        ret = self.commands.handle_msection(["A Test Title"], None, self.doc)
 
         self.assertEqual(ret, [])
 
-        last_header_elem = getattr(doc, "last_header_elem", None)
+        last_header_elem = getattr(self.doc, "last_header_elem", None)
 
         self.assertIsInstance(last_header_elem, pf.Header)
         self.assertIsInstance(last_header_elem.content[0], pf.RawInline)
         self.assertEqual(last_header_elem.content[0].text, "A Test Title")
         self.assertEqual(last_header_elem.identifier, "a-test-title")
         self.assertEqual(last_header_elem.level, 2)
+
+    def test_handle_msref(self):
+        ret = self.commands.handle_msref(['fooid', 'linktext'], None, self.doc)
+        self.assertIsInstance(ret, pf.Link)
+        self.assertEqual(ret.content[0].text, 'linktext')
+        self.assertEqual(ret.url, '#fooid')
