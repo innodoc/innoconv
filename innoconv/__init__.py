6d84bd8be63a338a21f7dfbd569d03e6e5c27791
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-02-26 15:42:42 +0100

    improve doc

--- innoconv/__init__.py
+++ innoconv/__init__.py
@@ -2,7 +2,7 @@
 This package translates mintmod-flavoured LaTex to vanilla LaTeX.
 
 It can be seen as a shim for ``mintmod.tex``. It handles important mintmod
-commands translating them to regular Pandoc elements. The central function in
-this module is the
-:class:`Pandoc filter <innoconv.mintmod_filter.filter_action.FilterAction>`.
+commands translating them to regular Pandoc elements. The central part in
+this package is
+:class:`FilterAction <innoconv.mintmod_filter.filter_action.FilterAction>`.
 """
