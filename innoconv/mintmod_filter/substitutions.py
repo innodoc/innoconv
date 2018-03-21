fdea6792d3a6cbbf4bbd4cbcb3a38515be044b38
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-03-21 17:19:12 +0100

    math substitutions

--- innoconv/mintmod_filter/substitutions.py
+++ innoconv/mintmod_filter/substitutions.py
@@ -1,20 +1,35 @@
 """Handle mintmod text substitution commands."""
 
+import re
+
+
 MATH_SUBSTITUTIONS = (
-    (r'\N', r'\mathbb{N}'),
-    (r'\Z', r'\mathbb{Z}'),
-    (r'\Q', r'\mathbb{Q}'),
-    (r'\R', r'\mathbb{R}'),
-    (r'\C', r'\mathbb{C}'),
-    (r'\Mtfrac', r'\tfrac'),
-    (r'\Mdfrac', r'\dfrac'),
-    (r'\MBlank', r'\ '),
-    (r'\MCondSetSep', r'{\,}:{\,}'),
+    # leave \Rightarrow, ... intact
+    (r'\\N($|[$^_,.\s])', r'\mathbb{N}\1'),
+    (r'\\Z($|[$^_,.\s])', r'\mathbb{Z}\1'),
+    (r'\\Q($|[$^_,.\s])', r'\mathbb{Q}\1'),
+    (r'\\R($|[$^_,.\s])', r'\mathbb{R}\1'),
+    (r'\\C($|[$^_,.\s])', r'\mathbb{C}\1'),
+
+    (r'\\Mtfrac{(.*?)}{(.*?)}', r'{\\textstyle \\frac{\1}{\2}}'),
+    (r'\\Mdfrac{(.*?)}{(.*?)}', r'{\\displaystyle \\frac{\1}{\2}}'),
+    (r'\\MBlank', r'\ '),
+    (r'\\MCondSetSep', r'{\,}:{\,}'),
+    (r'\\MDFPSpace', r'\,'),
+    (r'\\MDFPaSpace', r'\,\,'),
+    (r'\\MDFPeriod', r'\, .'),
+    (r'\\MSetminus', r'\setminus'),
+    (r'\\MElSetSep', ';'),
+    (r'\\MIntvlSep', ';'),
+    (r'\\MoIl', ']'),  # (#17)
+    (r'\\MoIr', '['),  # (#17)
+    (r'\\MEU', 'e'),
+    (r'\\MZahl{([0-9]*?)}{([0-9]*?)}', r'\1{,}\2')  # maybe obsolete (see #16)
 )
 
 
 def handle_math_substitutions(elem):
     """Handle simple mintmod text substitutions in math environments."""
     for repl in MATH_SUBSTITUTIONS:
-        elem.text = elem.text.replace(repl[0], repl[1])
+        elem.text = re.sub(repl[0], repl[1], elem.text)
     return elem
