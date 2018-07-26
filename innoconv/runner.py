b0307957d144e0b4ecb05b1d29032261308a64b7
Stefan Insam <insam@praktifix.math.tu-berlin.de>
2018-07-26 17:37:15 +0200

    added generating of toc.json, removed support for .tex

--- innoconv/runner.py
+++ innoconv/runner.py
@@ -2,11 +2,10 @@
 
 import json
 import os
+import yaml
 
 from innoconv.utils import to_ast, log
 
-
-# TODO: write toc.json
 
 # TODO: create post-process module system
 #  - copy static files
@@ -43,16 +42,72 @@ class InnoconvRunner():
             raise RuntimeError(
                 "Warning: Could not find language dir {}".format(language))
 
-        for root, dirs, files in os.walk(path):
-            rel_dir = root[len(self.source_dir):].lstrip('/')
-            dirs.sort()
-            for content_file_ext in ('md', 'tex'):
-                content_filename = 'content.{}'.format(content_file_ext)
-                if content_filename in files:
-                    self._process_file(rel_dir, content_filename)
+        tree = {}
 
-    def _process_file(self, rel_path, content_filename):
+        for root, dirs, files in os.walk(path):
+            rel_dir = root[len(self.source_dir):].lstrip(os.sep)
+
+            dirs.sort()
+
+            content_filename = 'content.{}'.format('md')
+            if content_filename in files:
+                self._process_file(rel_dir, content_filename, tree)
+
+        tree = self._folder_to_json_tree(tree)
+        log("Writing toc for {} to {}".format(
+            language, self.output_dir_base))
+        self._write_toc(language, tree)
+
+    def _folder_to_json_tree(self, tree):
+        newtree = []
+        for key in sorted(tree.keys()):
+            value = tree[key]
+            tree_object = {
+                'title': [{'t': 'Str', 'c': value[1]}],
+                'id': key
+            }
+            if value[0]:
+                tree_object['children'] = self._folder_to_json_tree(value[0])
+            newtree.append(tree_object)
+        return newtree
+
+    def _write_toc(self, language, tree):
+        out_path = os.path.join(self.output_dir_base, language)
+        out_path_filename = os.path.join(out_path, 'toc.json')
+        with open(out_path_filename, 'w') as out_file:
+            json.dump(tree, out_file)
+
+    def _process_file(self, rel_path, content_filename, tree):
+
+        def add_to_tree(tree, rel_path, full_path):
+            """Add a folder to the fodler tree
+
+            :param tree: The Object tree representign the File tree
+            :type tree: dict
+
+            :param rel_path: The relative path to the folder to add
+            :type rel_path: str
+
+            :param full_path: The full path to the content.md file to add
+            :type full_path: str
+
+            """
+            with open(full_path) as f_yaml:
+                for doc in yaml.safe_load_all(f_yaml):
+                    if doc and 'title' in doc:
+                        title = doc['title']
+                        break
+
+            folders = rel_path[3:].split(os.sep)
+            current = tree
+            for folder in folders:
+                if folder:
+                    if folder not in current:
+                        current[folder] = [{}, title]
+                    current = current[folder][0]
+
         full_path = os.path.join(self.source_dir, rel_path, content_filename)
+        add_to_tree(tree, rel_path, full_path)
         if os.path.isfile(full_path):
             log('processing {}'.format(full_path))
             ast = to_ast(full_path)
