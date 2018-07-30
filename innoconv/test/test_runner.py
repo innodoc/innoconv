ff9114c57a0b3d2d44f3e552561a5e429c02bfdb
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-30 18:13:48 +0200

    enable CI

---
+++ innoconv/test/test_runner.py
@@ -0,0 +1,16 @@
+"""Unit tests for innoconv.runner"""
+
+# pylint: disable=missing-docstring
+
+import unittest
+
+# supress linting until tests are implemented
+# pylint: disable=W0611
+from innoconv.runner import InnoconvRunner  # noqa: F401
+
+
+class TestInnoconvRunner(unittest.TestCase):
+
+    @unittest.skip("TBD")
+    def test_runner(self):
+        pass
