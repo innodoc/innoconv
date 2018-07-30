46bbb9c5037707364c356190e07e245095fbab80
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-30 18:13:48 +0200

    enable CI

---
+++ innoconv/test/test_main.py
@@ -0,0 +1,20 @@
+"""Unit tests for innoconv.__main__"""
+
+# pylint: disable=missing-docstring
+
+import unittest
+
+# supress linting until tests are implemented
+# pylint: disable=W0611
+from innoconv.__main__ import parse_cli_args, main  # noqa: F401
+
+
+class TestMain(unittest.TestCase):
+
+    @unittest.skip("TBD")
+    def test_parse_cli_args(self):
+        pass
+
+    @unittest.skip("TBD")
+    def test_main(self):
+        pass
