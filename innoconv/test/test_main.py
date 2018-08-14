3ffcc9244d24b6f7b70e5ce95410d6fb7a031a3e
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-08-14 13:28:48 +0200

    more unit tests tmp

--- innoconv/test/test_main.py
+++ innoconv/test/test_main.py
@@ -4,7 +4,9 @@
 
 import unittest
 
-# from innoconv.__main__ import get_arg_parser, main
+# supress linting until tests are implemented
+# pylint: disable=W0611
+from innoconv.__main__ import get_arg_parser, main  # noqa: F401
 
 
 class TestMain(unittest.TestCase):
