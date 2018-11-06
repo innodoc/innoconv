ad55e5f86dbb402814a180c4e188418c272c08a1
Stefan Insam <insam@math.tu-berlin.de>
2018-11-06 12:03:25 +0100

    Implemented Modularisation, closes #22

---
+++ innoconv/modules/demo/demo.py
@@ -0,0 +1,22 @@
+""" Demo for A module"""
+
+from innoconv.utils import log
+from innoconv.modloader import AbstractModule
+
+
+class Demo(AbstractModule):
+    """a Demo Module"""
+
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.events.extend([
+            'load_languages',
+            'pre_conversion',
+            'pre_language',
+            'pre_processing_veto',
+            'pre_content_file',
+            'process_ast',
+            'post_content_file',
+            'post_language',
+            'post_conversion'
+        ])
