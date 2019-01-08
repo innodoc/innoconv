8a04079f00045f76cbd93d121f5dfb28ca5309f5
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-01-08 16:26:40 +0100

    tikz TMP

--- innoconv/extensions/__init__.py
+++ innoconv/extensions/__init__.py
@@ -6,14 +6,14 @@ way. They can be enabled on a one-by-one basis.
 from innoconv.extensions.copy_static import CopyStatic
 from innoconv.extensions.generate_toc import GenerateToc
 from innoconv.extensions.join_strings import JoinStrings
-from innoconv.extensions.write_manifest import WriteManifest
 from innoconv.extensions.tikz2svg import Tikz2Svg
+from innoconv.extensions.write_manifest import WriteManifest
 
 #: List of available extensions
 EXTENSIONS = {
     'copy_static': CopyStatic,
     'generate_toc': GenerateToc,
     'join_strings': JoinStrings,
+    'tikz2svg': Tikz2Svg,
     'write_manifest': WriteManifest,
-    'tikz2svg': Tikz2Svg
 }
