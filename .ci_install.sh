6504ba386fa984907ff7f6f2e6ad882b6c548c7b
Stefan Insam <insam@math.tu-berlin.de>
2019-02-04 13:21:54 +0100

    fixed msitake

--- .ci_install.sh
+++ .ci_install.sh
@@ -23,22 +23,24 @@ pip install "flask==1.0.1" "CairoSVG==2.1.3"
 apk add texlive
 
 # Install necessary latex packages
-echo Installing Necessary LaTeX Packages
+echo Installing necessary LaTeX packages
 if ! [ -e .local/tex_packages ]; then
-  echo packages not present yet, downloading them
+  echo Packages not present yet, downloading them
   mkdir .local/tex_packages
   mkdir standalone
   cd standalone
   wget -q http://mirrors.ctan.org/install/macros/latex/contrib/standalone.tds.zip
-  unzip standalone.tds.zip
+  unzip -qq standalone.tds.zip
   cd ..
-  cp -rv standalone/* .local/tex_packages/
+  cp -r standalone/* .local/tex_packages/
   rm -rf standalone
   rm -f standalone.tds.zip
 fi
 
-ln -s .local/tex_packages `kpsewhich -var-value TEXMFHOME`
+mkdir `kpsewhich -var-value TEXMFHOME`
+cp -r .local/tex_packages/* `kpsewhich -var-value TEXMFHOME`/
 texhash `kpsewhich -var-value TEXMFHOME`
+echo Installed packages, using `kpsewhich standalone`
 
 # Install pdf2svg
 if [ -e .local/bin/pdf2svg ]; then
