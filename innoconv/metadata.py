502ec53f2c04f36abf95449b8eaaaf5f0646e173
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-03-13 17:18:40 +0100

    v0.0.2

--- innoconv/metadata.py
+++ innoconv/metadata.py
@@ -2,6 +2,6 @@
 
 __author__ = "Mirko Dietrich, innoCampus, TU Berlin"
 __author_email__ = "dietrich@math.tu-berlin.de"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __license__ = "GPLv3"
 __url__ = "https://gitlab.tu-berlin.de/innodoc/innoconv"
