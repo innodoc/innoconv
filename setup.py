5ea9ae13d36735b2e0935a87e4ab256258ce622c
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-03-14 21:51:05 +0100

    integrate tox (closes #29)

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 """Define project commands."""
 
-import distutils.cmd
-from distutils.command.clean import clean
 import os
 import logging
 import re
-import subprocess
+from shutil import rmtree
 import sys
-from setuptools import setup, find_packages
+from setuptools import Command, find_packages, setup
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-LINT_DIRS = [
-    os.path.join(ROOT_DIR, 'innoconv'),
-    os.path.join(ROOT_DIR, 'setup.py'),
-]
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
@@ -30,156 +24,34 @@ with open('README.md', 'r') as fh:
     LONG_DESCRIPTION = fh.read()
 
 
-def get_logger():
-    """Get logger instance for setup.py."""
-    return logging.getLogger('setup.py')
-
-
-class BaseCommand(distutils.cmd.Command):  # pylint: disable=no-member
-    """The base class for all custom commands."""
-
-    user_options = []
-
-    def __init__(self, *args, **kwargs):
-        """Get logger."""
-        super().__init__(*args, **kwargs)
-        self.log = get_logger()
-
-    def initialize_options(self):
-        """Set default values for options."""
-
-    def finalize_options(self):
-        """Post-process options."""
-
-    def _run(self, command, err_msg='Command failed!', cwd=ROOT_DIR):
-        self.log.info('Command %s', ' '.join(command))
-        proc = subprocess.Popen(command, cwd=cwd, stderr=subprocess.STDOUT)
-        return_code = proc.wait(timeout=240)
-        if return_code != 0:
-            raise RuntimeError(err_msg)
-
-    def _run_cmd(self, cmd):
-        self._run([sys.executable, 'setup.py'] + cmd)
-
-
-class Flake8Command(BaseCommand):
-    """Custom command that runs flake8 on source files."""
-
-    description = 'Run flake8 on Python source files.'
-
-    def run(self):
-        """Run command."""
-        self._run(['flake8', '--max-complexity=10'] + LINT_DIRS)
-
-
-class PylintCommand(BaseCommand):
-    """Custom command that runs pylint on source files."""
-
-    description = 'Run pylint on Python source files'
-
-    def run(self):
-        """Run command."""
-        self._run(['pylint', '--output-format=colorized'] + LINT_DIRS)
-
-
-class PyDocStyleCommand(BaseCommand):
-    """Custom command that runs pydocstyle on source files."""
-
-    description = 'Run pydocstyle on Python source files'
-
-    def run(self):
-        """Run command."""
-#        self._run(['pydocstyle', '--ignore=D401'] + LINT_DIRS)
-        self._run(['pydocstyle'] + LINT_DIRS)
-
-
-class LintCommand(BaseCommand):
-    """Custom command that runs all lint tools on source files."""
-
-    description = 'Run all lint tools on Python source files'
-
-    def run(self):
-        """Run command."""
-        self._run_cmd(['flake8'])
-        self._run_cmd(['pylint'])
-        self._run_cmd(['pydocstyle'])
-
-
-class TestCommand(BaseCommand):
-    """Custom command that runs unit tests on source files."""
-
-    description = 'Run unit test suite'
-
-    user_options = [
-        ('test-target=', 't', 'Test target (module or path)'),
-    ]
-
-    def initialize_options(self):
-        """Set default values for option for test_target."""
-        self.test_target = os.path.join(ROOT_DIR, 'test')
-
-    def run(self):
-        """Run command."""
-        self._run(['green', '-vv', '-r', self.test_target])
-
-
-class CoverageCommand(BaseCommand):
-    """Custom command that generates a HTML coverage report."""
-
-    description = 'Generate HTML coverage report'
-
-    def run(self):
-        """Run command."""
-        if not os.path.isfile(os.path.join(ROOT_DIR, '.coverage')):
-            self.log.error('No ".coverage" found. Running tests first…')
-            self._run_cmd(['test'])
-        self._run(['coverage', 'html'])
-
-
-class IntegrationTestCommand(BaseCommand):
-    """Custom command that runs integration tests on source files."""
-
-    description = 'Run integration test suite'
-
-    user_options = [
-        ('test-target=', 't', 'Test target (module or path)'),
-    ]
-
-    def initialize_options(self):
-        """Set default values for option for test_target."""
-        self.test_target = os.path.join(ROOT_DIR, 'integration_test')
-
-    def run(self):
-        """Run command."""
-        self._run(['green', '-vv', self.test_target])
-
-
-class CleanCommand(clean, BaseCommand):
-    """Custom command that cleans all build files."""
-
-    def run(self):
-        """Run command."""
-        super().run()
-        self._run(['rm', '-rf', os.path.join(ROOT_DIR, 'htmlcov')])
-        self._run(['rm', '-rf', os.path.join(ROOT_DIR, '.coverage')])
-        self._run(['rm', '-rf', os.path.join(ROOT_DIR, 'innoconv.egg-info')])
-        self._run(['rm', '-rf', os.path.join(ROOT_DIR, 'build')])
-        self._run(['rm', '-rf', os.path.join(ROOT_DIR, 'dist')])
-
-
-class UploadCommand(BaseCommand):
+class UploadCommand(Command):
     """Custom command that uploads release to PyPI and tags it in git."""
 
+    def initialize_options(self):
+        """Initialize options."""
+
+    def finalize_options(self):
+        """Finalize options."""
+
     def run(self):
         """Run command."""
-        self.log.info('Building distribution files (universal)…')
-        self._run_cmd(['clean'])
-        self._run_cmd(['sdist', 'bdist_wheel'])
-        self.log.info('Uploading the package to PyPI via Twine…')
-        self._run(['twine', 'upload', 'dist/*'])
-        self.log.info('Pushing git tag…')
-        self._run(['git', 'tag', f"v{METADATA['version']}"])
-        self._run(['git', 'push', '--tags'])
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(ROOT_DIR, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(METADATA['version']))
+        os.system('git push --tags')
+
+        sys.exit()
 
 
 def setup_package():
@@ -190,14 +62,6 @@ def setup_package():
         author=METADATA['author'],
         author_email=METADATA['author_email'],
         cmdclass={
-            'clean': CleanCommand,
-            'coverage': CoverageCommand,
-            'flake8': Flake8Command,
-            'integration_test': IntegrationTestCommand,
-            'pydocstyle': PyDocStyleCommand,
-            'lint': LintCommand,
-            'pylint': PylintCommand,
-            'test': TestCommand,
             'upload': UploadCommand,
         },
         description=METADATA['description'],
@@ -207,18 +71,21 @@ def setup_package():
             ],
         },
         extras_require={
-            'dev': [
-                'coverage',
-                'flake8',
-                'green',
-                'mccabe',
-                'pydocstyle',
-                'pylint',
-            ],
             'doc': [
                 'sphinx-click',
                 'sphinx-rtd-theme',
                 'Sphinx',
+            ],
+            'lint': [
+                'flake8',
+                'mccabe',
+                'pydocstyle',
+                'pylint',
+            ],
+            'test': [
+                'coverage',
+                'pytest',
+                'pytest-cov',
             ],
         },
         include_package_data=True,
@@ -232,7 +99,7 @@ def setup_package():
             'integration_test',
             'integration_test.*',
         ]),
-        python_requires='>=3.6.0',
+        python_requires='>=3.4.0',
         keywords=['innodoc', 'pandoc', 'markdown', 'education'],
         license=METADATA['license'],
         long_description=LONG_DESCRIPTION,
@@ -245,7 +112,6 @@ def setup_package():
         classifiers=[
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
             'License :: OSI Approved :: ' +
             'GNU General Public License v3 or later (GPLv3+)',
             'Operating System :: POSIX :: Linux',
@@ -262,5 +128,5 @@ if __name__ == '__main__':
     try:
         setup_package()
     except RuntimeError as err:
-        get_logger().error('%s: %s', type(err).__name__, err)
+        print(err)
         sys.exit(-1)
