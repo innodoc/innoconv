3ad0c61ff08f335619cc282806036a54945f5aa1
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-06-24 10:51:00 +0200

    v0.1.1

--- innoconv/metadata.py
+++ innoconv/metadata.py
@@ -2,7 +2,7 @@
 
 __author__ = "Mirko Dietrich"
 __author_email__ = "dietrich@math.tu-berlin.de"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "GPLv3"
 __url__ = "https://gitlab.tu-berlin.de/innodoc/innoconv"
 __description__ = "Converter for interactive educational content."
